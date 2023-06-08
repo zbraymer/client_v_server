c91ffa348ad1e0dfa9cd91e00e357f0b2ea1cb64
zbraymer <rayme1zb@gmail.com>
2023-06-08 08:45:17 -0400

    Updated client functionality

--- client.py
+++ client.py
@@ -2,34 +2,41 @@ import socket
 import threading
 
 
-def receive_time_from_server(client_socket):
+def receive_data_from_server(server_socket):
     while True:
-        time_data = client_socket.recv(1024).decode()
-        print(f"Received time from server: {time_data}")
+        # Receive data from the server
+        data = server_socket.recv(1024).decode()
+        print(f'\nReceived data from server: {data}\n')
 
 
-def run_client():
-    server_address = ('localhost', 12345)
+def start_client():
+    # Create a TCP/IP socket
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Connect the socket to the server's address and port
+    server_address = ('localhost', 12345)
     client_socket.connect(server_address)
+    print(f'Connected to the server at {server_address[0]}:{server_address[1]}')
 
-    print(f"Connected to {server_address[0]}:{server_address[1]}")
-
-    threading.Thread(target=receive_time_from_server, args=(client_socket,)).start()
+    # Create a separate thread to receive data from the server
+    receive_thread = threading.Thread(target=receive_data_from_server, args=(client_socket,))
+    receive_thread.start()
 
     while True:
-        message = input("Enter a message (or 'exit' to quit): ")
+        # Get user input
+        message = input('Enter text to send to the server (or type "exit" to quit): ')
 
-        if message == 'exit':
+        # If the user sends exit as an input, break the loop
+        if message.lower() == 'exit':
+            print('Exiting the client.')
             break
 
+        # Send user input to the server
         client_socket.send(message.encode())
-        response = client_socket.recv(1024).decode()
-        print(f"Server response: {response}")
 
-    print("Connection closed")
+    # Clean up the connection
     client_socket.close()
 
 
 if __name__ == '__main__':
-    run_client()
+    start_client()
